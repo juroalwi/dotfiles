1181d90a088dad3c48cbe5a5e1a969baaafcc605
Julian Alvarez <juroalwi@gmail.com>
2026-05-04 08:58:21 -0300

    add texmf and git links

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 DOTFILES="$HOME/dotfiles"
 
+ln -sf "$DOTFILES/git" "$HOME/.config/git"
 ln -sf "$DOTFILES/nvim" "$HOME/.config/nvim"
+ln -sf "$DOTFILES/texmf" "$HOME/texmf"
+
 echo "Symlinks created."
