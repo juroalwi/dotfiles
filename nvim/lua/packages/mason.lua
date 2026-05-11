1d6a2fc8650869de9098a052323c1bc2ef6637f0
Julian Alvarez <juroalwi@gmail.com>
2026-05-11 16:39:48 -0300

    enable julials on ftplugin file

--- nvim/lua/packages/mason.lua
+++ nvim/lua/packages/mason.lua
@@ -4,9 +4,10 @@ require("mason-lspconfig").setup({
   automatic_enable = {
     exclude = {
       "julials"
+      "efm",     -- Already using none-ls, do not auto enable efm.
     }
   },
-  ensure_installed = { "efm", "lua_ls" },
+  ensure_installed = { "lua_ls" },
 })
 
 vim.lsp.enable("julials")
