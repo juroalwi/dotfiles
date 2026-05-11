974db74ba8f24601500027619a2d0049b26416f5
Julian Alvarez <juroalwi@gmail.com>
2026-05-11 17:42:47 -0300

    use prettierd, remove stylua

--- nvim/lua/packages/null-ls.lua
+++ nvim/lua/packages/null-ls.lua
@@ -3,9 +3,8 @@ local null_ls = require("null-ls")
 null_ls.setup({
   debug = true,
   sources = {
-    null_ls.builtins.formatting.stylua,
     null_ls.builtins.completion.spell,
-    null_ls.builtins.formatting.prettier,
+    null_ls.builtins.formatting.prettierd,
     require("none-ls.diagnostics.eslint_d").with({
       env = { ESLINT_USE_FLAT_CONFIG = "false" },
       ignore_stderr = true,
