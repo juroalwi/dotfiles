fba0215fad75174ea2e67d6a08f1bf14021417fb
Julian Alvarez <juroalwi@gmail.com>
2026-05-14 20:27:17 -0300

    treesitter auto install

--- nvim/lua/packages/treesitter.lua
+++ nvim/lua/packages/treesitter.lua
@@ -1,9 +1,28 @@
 require("nvim-treesitter").setup()
+
 vim.api.nvim_create_autocmd("FileType", {
   callback = function()
-    pcall(vim.treesitter.start) -- Highlighting.
-    vim.bo.indentexpr = (       -- Indentation.
-      "v:lua.require('nvim-treesitter').indentexpr()"
-    )
+    -- Highlight.
+    pcall(vim.treesitter.start)
+
+    -- Indentation.
+    vim.bo.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
+
+    -- Auto install.
+    local ensureInstalled = {
+      "lua",
+      "python",
+      "typescript",
+      "html",
+      "css",
+      "json"
+    }
+    local alreadyInstalled = require("nvim-treesitter.config").get_installed()
+    local parsersToInstall = vim.iter(ensureInstalled)
+        :filter(function(parser)
+          return not vim.tbl_contains(alreadyInstalled, parser)
+        end)
+        :totable()
+    require("nvim-treesitter").install(parsersToInstall)
   end,
 })
