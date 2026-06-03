12af26e5784016120f6595fed1a8c9741b45b2af
Julian Alvarez <juroalwi@gmail.com>
2026-06-02 22:54:18 -0300

    mini pick: update keymaps

--- nvim/lua/packages/mini-pick.lua
+++ nvim/lua/packages/mini-pick.lua
@@ -4,17 +4,20 @@ local map = require("utils.keymaps").map
 
 pick.setup({
   mappings = {
+    paste            = "",
+    choose           = "",
+    refine           = "",
+    choose_marked    = "",
+    refine_marked    = "",
     choose_smart     = {
       char = "<CR>",
       func = function()
         local matches = pick.get_picker_matches()
         local opts = pick.get_picker_opts()
         if matches.marked and #matches.marked > 0 then
-          local choose_marked = opts.source.choose_marked or pick.default_choose_marked
-          choose_marked(matches.marked)
+          (opts.source.choose_marked or pick.default_choose_marked)(matches.marked)
         elseif matches.current then
-          local choose = opts.source.choose or pick.default_choose
-          choose(matches.current)
+          (opts.source.choose or pick.default_choose)(matches.current)
         end
         return true
       end,
@@ -31,26 +34,18 @@ pick.setup({
     custom_move_down = {
       char = "<C-n>",
       func = function()
-        vim.api.nvim_feedkeys(vim.keycode('<TAB>', true, true, true), 'n', false)
+        vim.api.nvim_feedkeys(vim.keycode("<TAB>", true, true, true), "n", false)
       end,
     },
     custom_move_up   = {
       char = "<C-p>",
       func = function()
-        vim.api.nvim_feedkeys(vim.keycode('<S-TAB>', true, true, true), 'n', false)
+        vim.api.nvim_feedkeys(vim.keycode("<S-TAB>", true, true, true), "n", false)
       end,
     },
 
-    paste            = "",
-
     move_down        = "<TAB>",
     move_up          = "<S-TAB>",
-
-    choose           = "<C-y>",
-    choose_marked    = "<M-y>",
-    refine           = "<C-f>",
-    refine_marked    = "<M-f>",
-
     stop             = "<C-q>",
     mark             = "<C-x>",
     mark_all         = "<C-a>",
@@ -59,14 +54,16 @@ pick.setup({
     toggle_preview   = "<C-e>",
     delete_word      = "<C-w>",
     delete_left      = "<C-u>",
-
     scroll_left      = "<C-h>",
     scroll_down      = "<C-j>",
     scroll_up        = "<C-k>",
     scroll_right     = "<C-l>",
   },
   window = {
-    prompt_prefix = " ",
+    config = function()
+      return { width = vim.o.columns }
+    end,
+    prompt_prefix = " ",
   },
 })
 
@@ -81,17 +78,22 @@ pick.registry.files_hidden = function()
     "--glob", "!**/dist/*",
     "--glob", "!**/build/*",
     "--glob", "!**/coverage/*",
+    "--glob", "!**/.jest-cache/*",
     "--glob", "!**/.next/*",
     "--glob", "!**/.cache/*",
   }
   return pick.builtin.cli({ command = command })
 end
 
-map("n", "<LEADER>f", function()
+map("n", "<LEADER>p", function()
+  vim.cmd("Pick files")
+end, { noremap = true, silent = true, unique = true })
+
+map("n", "<LEADER>h", function()
   vim.cmd("Pick files_hidden")
 end, { noremap = true, silent = true, unique = true })
 
-map("n", "<LEADER>s", function()
+map("n", "<LEADER>f", function()
   vim.cmd("Pick grep_live")
 end, { noremap = true, silent = true, unique = true })
 
