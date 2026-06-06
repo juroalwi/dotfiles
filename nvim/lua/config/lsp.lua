1f7230b5ff18b8b7e1bbe03d9bbc51863cd6f7ad
Julian Alvarez <juroalwi@gmail.com>
2026-06-06 17:52:27 -0300

    use mini pick for lsp actions

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,8 +1,18 @@
 local map = require("utils.keymaps").map
 
 local lsp_group = vim.api.nvim_create_augroup("PoiLspSetup", { clear = true })
-local format_group = vim.api.nvim_create_augroup("PoiFormat", { clear = true })
+local format_group = vim.api.nvim_create_augroup("PoiLspFormat", { clear = true })
 local highlight_group = vim.api.nvim_create_augroup("PoiLspHighlight", { clear = true })
+
+local format_on_save_enabled = false
+map("n", "<LEADER>t", function()
+  format_on_save_enabled = not format_on_save_enabled
+  vim.notify("Format on save " .. (format_on_save_enabled and "enabled" or "disabled"))
+end)
+
+map("n", "<LEADER>l", function()
+  vim.cmd.edit(vim.lsp.get_log_path())
+end)
 
 vim.api.nvim_create_autocmd("LspAttach", {
   group = lsp_group,
@@ -14,37 +24,68 @@ vim.api.nvim_create_autocmd("LspAttach", {
       return
     end
 
-    -- Code formatting.
-    if client.server_capabilities.documentFormattingProvider then
-      map({ "n", "x" }, "gq", function()
-        vim.lsp.buf.format({
-          bufnr = args.buf,
-          async = false,
-        })
-        vim.api.nvim_feedkeys(
-          vim.api.nvim_replace_termcodes("<ESC>", true, false, true),
-          "n",
-          true
-        )
-      end, { buffer = args.buf, unique = false })
+    local lsp_map = function(mode, lhs, rhs)
+      return map(mode, lhs, rhs, { buffer = args.buf, unique = false, nowait = true })
+    end
 
+    local with_mini_pick = function(cmd, fallback)
+      return function()
+        local ok, mini_extra = pcall(require, "mini.extra")
+        if ok then
+          mini_extra.pickers.lsp({ scope = cmd })
+        else
+          fallback()
+        end
+      end
+    end
+
+    if client:supports_method("textDocument/definition") then
+      lsp_map("n", "gd", with_mini_pick("definition", vim.lsp.buf.definition))
+    end
+
+    if client:supports_method("textDocument/typeDefinition") then
+      lsp_map("n", "gt", with_mini_pick("type_definition", vim.lsp.buf.type_definition))
+    end
+
+    if client:supports_method("textDocument/implementation") then
+      lsp_map("n", "gi", with_mini_pick("implementation", vim.lsp.buf.implementation))
+    end
+
+    if client:supports_method("textDocument/references") then
+      lsp_map("n", "gr", with_mini_pick("references", vim.lsp.buf.references))
+    end
+
+    if client:supports_method("textDocument/rename") then
+      lsp_map("n", "gn", vim.lsp.buf.rename)
+    end
+
+    if client:supports_method("textDocument/code_action") then
+      lsp_map("n", "ga", vim.lsp.buf.code_action)
+    end
+
+    if client:supports_method("textDocument/formatting") then
+      lsp_map("n", "gq", vim.lsp.buf.format)
+      lsp_map("x", "gq", function()
+        vim.lsp.buf.format()
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<ESC>", true, false, true), "n", true)
+      end)
+    end
+
+    -- Format on save.
+    if client.server_capabilities.documentFormattingProvider then
       vim.api.nvim_create_autocmd("BufWritePre", {
         group = format_group,
         buffer = args.buf,
         callback = function()
+          if not format_on_save_enabled then
+            return
+          end
           vim.lsp.buf.format({
             bufnr = args.buf,
             async = false,
           })
         end,
       })
-    end
-
-    -- Go to definition.
-    if client.server_capabilities.definitionProvider then
-      map("n", "gd", function()
-        vim.lsp.buf.definition()
-      end, { buffer = args.buf, unique = false })
     end
 
     -- References highlighting.
