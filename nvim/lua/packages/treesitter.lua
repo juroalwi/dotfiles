1f9f1a45fb7f54c69b0978b818a11419735d6e0f
Julian Alvarez <juroalwi@gmail.com>
2026-07-05 20:38:01 -0300

    remove treesitter indentation

--- nvim/lua/packages/treesitter.lua
+++ nvim/lua/packages/treesitter.lua
@@ -5,23 +5,21 @@ vim.api.nvim_create_autocmd("FileType", {
     -- Highlight.
     pcall(vim.treesitter.start)
 
-    -- Indentation.
-    vim.bo.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
-
     -- Auto install.
     local ensureInstalled = {
       "lua",
       "python",
       "typescript",
+      "javascript",
+      "tsx",
+      "jsx",
       "html",
       "css",
       "json"
     }
     local alreadyInstalled = require("nvim-treesitter.config").get_installed()
     local parsersToInstall = vim.iter(ensureInstalled)
-        :filter(function(parser)
-          return not vim.tbl_contains(alreadyInstalled, parser)
-        end)
+        :filter(function(parser) return not vim.tbl_contains(alreadyInstalled, parser) end)
         :totable()
     require("nvim-treesitter").install(parsersToInstall)
   end,
