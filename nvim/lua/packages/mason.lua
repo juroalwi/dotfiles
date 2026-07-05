e303a52c972d335249f2fa7a8ba480969cb48b4c
Julian Alvarez <juroalwi@gmail.com>
2026-07-05 20:53:07 -0300

    remove efm from auto install

--- nvim/lua/packages/mason.lua
+++ nvim/lua/packages/mason.lua
@@ -4,7 +4,6 @@ require("mason-lspconfig").setup({
   automatic_enable = {
     exclude = {
       "julials", -- Manually start julials on ftplugin/julia.lua to avoid lsp crash (known Mason issue).
-      "efm",     -- Already using none-ls, do not auto enable efm.
     }
   },
   ensure_installed = {
