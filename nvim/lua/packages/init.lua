f426b229cc0614807e8f303e83e9758c1ba3b243
Julian Alvarez <juroalwi@gmail.com>
2026-07-05 20:53:28 -0300

    add undootree, add mini pairs, go back to blink.cmp v1

--- nvim/lua/packages/init.lua
+++ nvim/lua/packages/init.lua
@@ -1,16 +1,16 @@
 local pack = require("utils.packages")
 
+vim.cmd('packadd nvim.undotree')
+
 pack.add({
   { "catppuccin/nvim" },
+  { "nvim-treesitter/nvim-treesitter", require = "packages.treesitter" },
   { "neovim/nvim-lspconfig" },
   { "mason-org/mason.nvim",            require = "packages.mason" },
   { "mason-org/mason-lspconfig.nvim" },
   { "nvimtools/none-ls.nvim",          require = "packages.null-ls" },
   { "nvimtools/none-ls-extras.nvim" },
-  { "saghen/blink.cmp",                require = "packages.blink-cmp" },
-  { "saghen/blink.lib" },
-  { "windwp/nvim-autopairs",           setup = true },
-  { "windwp/nvim-ts-autotag",          setup = true },
+  { "nvim-mini/mini.pairs",            setup = true },
   { "nvim-mini/mini.extra",            setup = true },
   { "nvim-mini/mini.icons",            setup = true },
   { "nvim-mini/mini.indentscope",      setup = true },
@@ -19,9 +19,10 @@ pack.add({
   { "nvim-mini/mini.pick",             require = "packages.mini-pick" },
   { "nvim-mini/mini.diff",             require = "packages.mini-diff" },
   { "nvim-mini/mini.surround",         require = "packages.mini-surround" },
+
   { "nvim-lua/plenary.nvim" },
+  { "saghen/blink.cmp",                require = "packages.blink-cmp",     version = "v1" },
   { "olimorris/codecompanion.nvim",    require = "packages.codecompanion", version = vim.version.range("^19.0.0") },
-  { "nvim-treesitter/nvim-treesitter", require = "packages.treesitter" },
   { "tpope/vim-fugitive",              require = "packages.fugitive" },
 })
 
